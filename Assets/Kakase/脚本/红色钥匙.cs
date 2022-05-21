04160325c31094d38290a5737ad8bace2b1b1256
citrusKakase <751832214@qq.com>
2022-05-21 15:08:51 +0800

    v0.13
    抽象出道具类，以后任何道具都需要先继承这个类

--- Assets/Kakase/脚本/红色钥匙.cs
+++ Assets/Kakase/脚本/红色钥匙.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class 红色钥匙 : 道具
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
--- Assets/Kakase/脚本/道具.cs
+++ Assets/Kakase/脚本/道具.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class 道具 :MonoBehaviour
+public class 道具 : MonoBehaviour
 {
     public int 道具ID;
     public ItemManager 道具管理器;
     public Text 旁白系统;
     public string 道具名;
 
-    public void 拾取()
+    [Range(0,1)][Tooltip("0是可拾取道具，1是可交互道具")][SerializeField]
+    private int 道具类型;
+
+    public void 被交互()
+    {
+        switch(道具类型)
+        {
+            case 0:
+                可交互道具();
+                break;
+            case 1:
+                可交互道具();
+                break;
+        }
+    }
+
+
+    public void 可拾取道具()
     {
         旁白系统.SendMessage("ShowDialog", "获得了" + 道具名);
         道具管理器.获得道具(道具ID);
         Destroy(gameObject);
     }
 
+
+
+    public void 可交互道具()
+    {
+
+    }
 }
