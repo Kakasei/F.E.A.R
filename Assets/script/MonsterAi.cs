42f451ec2edede3d11fe45b057a5422181d4aeca
retreat0021 <937380266@qq.com>
2022-05-22 22:55:05 +0800

    1
    僵尸跟踪以及回归巡逻状态

--- Assets/script/MonsterAi.cs
+++ Assets/script/MonsterAi.cs
@@ -13,42 +13,78 @@ public class MonsterAi : MonoBehaviour
     public Animator ani;
     private Transform player;
     public NavMeshAgent agent;
-    AnimatorStateInfo info;
+ //   AnimatorStateInfo info;
+    private float 旋转坐标 = 0;
+    private bool isFollowAction = false;
+    private bool isPatrolAction = true;
+    public Vector3 waitPosition;
     // Start is called before the first frame update
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
+        double stopdistance = 1.5;
+  //    info = ani.GetCurrentAnimatorStateInfo(0);
+        float distance = Vector3.Distance(player.position, transform.position);
         Vector3 v = player.position;
         if (other.gameObject.tag == "Player")
         {
             this.gameObject.GetComponent<Animator>().SetBool("run", true);
+            isPatrolAction = false;
+            isFollowAction = true;
             transform.LookAt(v);
             agent.speed = 2f;
             agent.isStopped = false;
             agent.SetDestination(player.position);
+
+            //追上主角 主角die
+            if (distance <= stopdistance)
+            {
+                this.gameObject.GetComponent<Animator>().SetBool("run", false);
+                agent.isStopped = true;
+            }
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //回到巡逻点
+        if(other.gameObject.tag == "Player")
+        {
+            transform.LookAt(waitPosition);
+            agent.SetDestination(waitPosition);
+            agent.speed = 0.1f;
+            isFollowAction = false;
+        }
+    }
+
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
+        waitPosition = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 v = player.position;
-        double stopdistance = 1.5;
-        info = ani.GetCurrentAnimatorStateInfo(0);
-        float distance = Vector3.Distance(player.position, transform.position);
-
-        
-        if(distance <= stopdistance)
+        if (isPatrolAction)
         {
-            this.gameObject.GetComponent<Animator>().SetBool("run", false);
-            
+            transform.rotation = Quaternion.Euler(new Vector3(0, 旋转坐标++, 0));
+            if (旋转坐标 >= 360) 旋转坐标 -= 360;
+        }
+        else
+        {
+            if (isFollowAction)
+            {
+
+            }
+            else if (System.Math.Abs(waitPosition.sqrMagnitude - this.transform.position.sqrMagnitude) < 5)
+            {
+                this.gameObject.GetComponent<Animator>().SetBool("run", false);
+                agent.isStopped = true;
+                isPatrolAction = true;
+            }  
         }
         
-
 
         /*switch (CurrentState)
         {
