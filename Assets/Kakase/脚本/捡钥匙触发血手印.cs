b818ea3493b54f04f3c9be702fb9394e5839c175
citrusKakase <751832214@qq.com>
2022-05-22 21:31:39 +0800

    v0.139
    触发血手印基本完成

--- Assets/Kakase/脚本/捡钥匙触发血手印.cs
+++ Assets/Kakase/脚本/捡钥匙触发血手印.cs
@@ -7,22 +7,40 @@ public class 捡钥匙触发血手印 : MonoBehaviour
     private Transform[] 血手印集;
     private AudioSource 音频源;
 
+
     private void Awake()
     {
         血手印集 = GetComponentsInChildren<Transform>(true);
         音频源 = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void 触发血手印()
     {
-        Debug.Log(血手印集[1].name);
-        血手印集[1].gameObject.SetActive(true);
-        音频源.Play();
+        StartCoroutine(血脚印出现());
+    }
+
+    private IEnumerator 血手印出现()
+    {
+        for(int i = 1; i <= 3; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Debug.Log(血手印集[i].name);
+            血手印集[i].gameObject.SetActive(true);
+            音频源.Play();
+        }
+    }
+
+    private IEnumerator 血脚印出现()
+    {
+        yield return StartCoroutine(血手印出现());
+
+        for (int i = 4; i <= 7; i++)
+        {
+            yield return new WaitForSeconds(1.0f);
+            Debug.Log(血手印集[i].name);
+            血手印集[i].gameObject.SetActive(true);
+            音频源.Play();
+        }
     }
 }
