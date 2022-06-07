d1e48166792769da79fb3bda56de9b129e3a216b
citrusKakase <751832214@qq.com>
2022-06-08 01:40:55 +0800

    拾取音效

--- Assets/Kakase/脚本/红色钥匙.cs
+++ Assets/Kakase/脚本/红色钥匙.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class 红色钥匙 : 道具
 {
     public 捡钥匙触发血手印 捡钥匙触发血手印_脚本;
+    private AudioSource 音频源;
 
 
     public override void 拾取后触发()
     {
         base.拾取后触发();
+        音频源.Play();
         捡钥匙触发血手印_脚本.触发血手印();
     }
 }
---
+++ Assets/Kakase/脚本/纸条.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class 纸条 : 道具
+{
+    private AudioSource 音频源;
+
+
+    private void Awake()
+    {
+        音频源 = GetComponent<AudioSource>();
+    }
+
+    public override void 拾取后触发()
+    {
+        base.拾取后触发();
+        音频源.Play();
+
+        
+    }
+}
