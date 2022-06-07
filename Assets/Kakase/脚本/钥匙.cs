31e9086a9afe5074dcd8bc09639aafccb3037477
retreat0021 <937380266@qq.com>
2022-06-08 01:49:12 +0800

    111

--- Assets/Kakase/脚本/钥匙.cs
+++ Assets/Kakase/脚本/钥匙.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 红色钥匙 : 道具
+[RequireComponent(typeof(AudioSource))]
+public class 钥匙 : 道具
 {
-    public 捡钥匙触发血手印 捡钥匙触发血手印_脚本;
     private AudioSource 音频源;
 
+
+    private void Awake()
+    {
+        音频源 = GetComponent<AudioSource>();
+    }
 
     public override void 拾取后触发()
     {
         base.拾取后触发();
         音频源.Play();
-        捡钥匙触发血手印_脚本.触发血手印();
     }
+
+
 }
