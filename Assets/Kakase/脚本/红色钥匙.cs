b2c0d3ad888568e429db219071b1db9e1146dac3
citrusKakase <751832214@qq.com>
2022-06-08 02:03:56 +0800

    小修bug

--- Assets/Kakase/脚本/红色钥匙.cs
+++ Assets/Kakase/脚本/红色钥匙.cs
@@ -7,6 +7,12 @@ public class 红色钥匙 : 道具
     public 捡钥匙触发血手印 捡钥匙触发血手印_脚本;
     private AudioSource 音频源;
 
+    private void Awake()
+    {
+        音频源 = GetComponent<AudioSource>();
+    }
+
+
 
     public override void 拾取后触发()
     {
