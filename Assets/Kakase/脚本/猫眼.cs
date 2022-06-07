ab4de51b791d326a1ecece57e402087d5bc5d3a6
citrusKakase <751832214@qq.com>
2022-06-07 15:32:10 +0800

    123

--- Assets/Kakase/脚本/猫眼.cs
+++ Assets/Kakase/脚本/猫眼.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class 猫眼 : 道具
 {
+    public RawImage 猫眼鬼图;
 
+    public BoxCollider 碰撞体;
+
+    private void Start()
+    {
+        碰撞体 = this.gameObject.GetComponent<BoxCollider>();
+        
+    }
+
+    public override void 交互后触发()
+    {
+        猫眼鬼图.enabled = true;
+        碰撞体.enabled = false;
+        StartCoroutine(鬼图播放完后删除());
+
+    }
+
+    private IEnumerator 鬼图播放完后删除()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Debug.Log(this.name + "现在被彻底删除了");
+        Destroy(this.gameObject);
+    }
 }
--- Assets/Kakase/脚本/马桶里的道具.cs
+++ Assets/Kakase/脚本/马桶里的道具.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class 马桶里的道具 : 道具
 {
     public 无锁门 厕所门脚本;
+    public 猫眼 猫眼脚本;
+
     private void Start()
     {
         
@@ -15,10 +17,16 @@ public class 马桶里的道具 : 道具
     public override void 拾取后触发()
     {
         base.拾取后触发();
+
+        //拾取到马桶里的道具后，调用厕所门和猫眼脚本，使厕所门锁死，猫眼鬼图模块激活
         厕所门脚本.旋转速度 = 5.0f;
         厕所门脚本.被交互();
         厕所门脚本.锁死 = true;
         StartCoroutine(锁门());
+
+        猫眼脚本.碰撞体.enabled = true;
+
+
     }
     
     public IEnumerator 锁门()
